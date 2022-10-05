eee1e97debb651b606709f11ad04897d379ee32f
Manuel Frühstück <manuel.fruehstueck@stud.fh-campuswien.ac.at>
2022-10-05 19:39:09 +0200

    Solved Task 1

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -4,37 +4,70 @@ import java.util.Scanner;
 
 public class App {
 
+    // Global vars
+    Scanner sc = new Scanner(System.in);
+
     //todo Task 1
-    public void largestNumber(){
+    public void largestNumber() {
         // input your solution here
+
+        double input;
+        double largestNum = 0;
+        int numCnt = 0;
+
+        do {
+            System.out.printf("Number %d: ", numCnt + 1);
+            input = sc.nextDouble();
+
+            // Check for invalid input
+            if (numCnt < 1 && input <= 0) {
+                System.out.println("No number entered.");
+                return;
+            }
+
+            // Check for stop-condition
+            if (input > 0){
+
+                // Check for new largestNum and increment numCnt
+                if (input > largestNum) {
+                    largestNum = input;
+                }
+                numCnt++;
+            } else break;
+
+        } while (true);
+
+        System.out.printf("The largest number is %.2f" + System.lineSeparator(), largestNum);
     }
 
     //todo Task 2
-    public void stairs(){
+    public void stairs() {
         // input your solution here
+
+
     }
 
     //todo Task 3
-    public void printPyramid(){
+    public void printPyramid() {
         // input your solution here
     }
 
     //todo Task 4
-    public void printRhombus(){
+    public void printRhombus() {
         // input your solution here
     }
 
     //todo Task 5
-    public void marks(){
+    public void marks() {
         // input your solution here
     }
 
     //todo Task 6
-    public void happyNumbers(){
+    public void happyNumbers() {
         // input your solution here
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         App exercise2 = new App();
 
         System.out.println("Task 1: Largest Number");
